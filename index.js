e3ca7c7a14124ed962109d27cd5bf8bb74c962fd
dfghjkcvbnmnbvcx <163096242+kbkhkj@users.noreply.github.com>
2024-03-17 15:26:47 +0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+function generateRandomPassword(length) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+  }
+  
+  const passwordLength = 12;
+  const password = generateRandomPassword(passwordLength);
+  console.log('Random password:', password);
+  
